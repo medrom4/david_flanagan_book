da7dfd90a1f5c7665deb55c014dd97df9ff607f4
Ramanovich <medikroma@gmail.com>
2018-10-11 17:19:06 +0000

    oop

--- script/script.js
+++ script/script.js
@@ -1,10 +1,13 @@
-function factorial(x) {
-    var product = 1;
-    while (x > 1) {
-        product *= x;
-        x--;
-    }
-    return product;
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
 }
+var p = new Point(1, 1);
 
-console.log(factorial(3));
+Point.prototype.r = function() {
+    return Math.sqrt(
+        this.x * this.x + this.y * this.y    
+    );
+};
+
+console.log(p.r());
