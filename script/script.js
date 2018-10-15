fff3856581ac4e036395a822e605cc92a703c425
Ramanovich <medikroma@gmail.com>
2018-10-15 12:12:08 +0000

    js_var

---
+++ script/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var amount = document.getElementById("amount");
+var apr = document.getElementById("apr");
+var years = document.getElementById("years");
+var zipcode = document.getElementById("zipcode");
+var payment = document.getElementById("payment");
+var total = document.getElementById("total");
+var totalinterest = document.getElementById("totalinterest");
