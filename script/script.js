249f31c8c9bbf757d200a8ce9fb37978cd06d404
Ramanovich <medikroma@gmail.com>
2018-10-24 10:34:56 +0000

    switch_js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,17 @@
 'use strict';
 
-var a = 1;
-var b = " ";
+var n = '23';
 
-console.log(a + b);
-
+switch (n) {
+    case 'one':
+        console.log('Первый CASE');
+        break;
+    case 'two':
+        console.log('Второй CASE');
+        break;
+    case 'three':
+        console.log('Третий CASE');
+        break;
+    default:
+        alert('Ни первый ни второй ни третий CASE не срабтал, увы и ах');
+}
