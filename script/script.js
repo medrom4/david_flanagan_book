6842bd2fbbb90af167d6ea1d44cda163a75fb8d6
Ramanovich <medikroma@gmail.com>
2018-10-24 10:45:52 +0000

    for_in

--- script/script.js
+++ script/script.js
@@ -1,17 +1,9 @@
 'use strict';
 
-var n = '23';
+var o = { x: 1, y: 2, z: 3 };
+var a = [];
+var i = 0;
 
-switch (n) {
-    case 'one':
-        console.log('Первый CASE');
-        break;
-    case 'two':
-        console.log('Второй CASE');
-        break;
-    case 'three':
-        console.log('Третий CASE');
-        break;
-    default:
-        alert('Ни первый ни второй ни третий CASE не срабтал, увы и ах');
+for (a[i++] in o) {
+    console.log(a);
 }
